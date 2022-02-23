050ee0ec65e58db4aeb00a7ea7ac78fc0ad15fec
bence056 <vargabencecsaba2005@gmail.com>
2022-02-23 14:24:14 +0100

    topejtrpiuwjpiuojyer

--- Source/SessionHandlingSteam/CustomGameInstance.cpp
+++ Source/SessionHandlingSteam/CustomGameInstance.cpp
@@ -109,7 +109,7 @@ void UCustomGameInstance::JoinServer()
 	SessionSearch->bIsLanQuery = false;
 	SessionSearch->MaxSearchResults = 10000;
 
-	SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, false, EOnlineComparisonOp::Equals);
+	SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
 	
 	// SessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
 	// SessionSearch->QuerySettings.Set(SEARCH_DEDICATED_ONLY, true, EOnlineComparisonOp::Equals);
